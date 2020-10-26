626d3915028d6b7ae6ec29859d4843e2a8e3045a
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2020-10-26 18:48:17 +0530

    Updated Dependencies

--- src/Hangcord.js
+++ src/Hangcord.js
@@ -2,6 +2,25 @@
  * Hangcord game generator
  */
 
- class HangmanGame{};
+const possibleWords = require('./utils/words.json');
 
- module.exports = HangmanGame;
+const letterEmojisMap = {
+    "🅰️": "A", "🇦": "A", "🅱️": "B", "🇧": "B", "🇨": "C", "🇩": "D", "🇪": "E",
+    "🇫": "F", "🇬": "G", "🇭": "H", "ℹ️": "I", "🇮": "I", "🇯": "J", "🇰": "K", "🇱": "L",
+    "Ⓜ️": "M", "🇲": "M", "🇳": "N", "🅾️": "O", "⭕": "O", "🇴": "O", "🅿️": "P",
+    "🇵": "P", "🇶": "Q", "🇷": "R", "🇸": "S", "🇹": "T", "🇺": "U", "🇻": "V", "🇼": "W",
+    "✖️": "X", "❎": "X", "❌": "X", "🇽": "X", "🇾": "Y", "💤": "Z", "🇿": "Z"
+}
+
+class HangmanGame{
+
+    constructor(options){
+        this.words = possibleWords;
+        this.options = {
+            ...options
+        };
+    };
+
+};
+
+module.exports = HangmanGame;
