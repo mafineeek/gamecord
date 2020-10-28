3e42d87e09b421dd4882a76770ad7a65a2634861
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2020-10-28 20:38:22 +0530

    Updated utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,4 @@
 const random = arr => arr[Math.floor(Math.random() * arr.length)];
 
-module.exports = {
-    words: require('./words.json'),
-    quiz: word => word.split('').map(x => random([true, false]) ? '_' : x),
-    bool: _ => Math.floor(Math.random() * 2),
-    random
-};
+module.exports.random = random;
+module.exports.words = require('./words.json');
